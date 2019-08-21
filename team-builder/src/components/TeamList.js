e64356363eeca2b0f7c545e52f0feafeb7ca0902
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-21 12:35:38 -0600

    TeamList set up

---
+++ team-builder/src/components/TeamList.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const TeamList = props =>{
+  console.log(props);
+  return(
+    <div className="team-list">
+      {props.teamList.map(team =>{
+        return(
+          <div className="team" key={team.id}>
+            <h2>{team.name}</h2>
+            <p>{team.email}</p>
+            <p>{team.role}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
